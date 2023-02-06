1ce2288335b861f35c9d23951f0205080a339968
hind-achniri <achniri2000@gmail.com>
2023-02-07 00:37:53 +0100

    Ajout de fichiers supplementaires

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM node:18.13.0 as node
+WORKDIR /app
+COPY . .
+RUN npm install
+RUN npm run build --prod
+# Stage 2
+FROM nginx:alpine
+COPY --from=node /app/dist/my-app /usr/share/nginx/html
---
+++ api.module.ts
@@ -0,0 +1,39 @@
+import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import { Configuration } from './configuration';
+import { HttpClient } from '@angular/common/http';
+
+
+import { AdvertiserControllerService } from './api/advertiserController.service';
+import { PlaylistControllerService } from './api/playlistController.service';
+import { SearchControllerService } from './api/searchController.service';
+import { UserControllerService } from './api/userController.service';
+
+@NgModule({
+  imports:      [],
+  declarations: [],
+  exports:      [],
+  providers: [
+    AdvertiserControllerService,
+    PlaylistControllerService,
+    SearchControllerService,
+    UserControllerService ]
+})
+export class ApiModule {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
+        return {
+            ngModule: ApiModule,
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+        };
+    }
+
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
+                 @Optional() http: HttpClient) {
+        if (parentModule) {
+            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+        }
+        if (!http) {
+            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+            'See also https://github.com/angular/angular/issues/20575');
+        }
+    }
+}
